1179d88f1d70aacf751a560655b3ca35101d9764
Kaustubh <kasutubhbawa123@gmail.com>
2023-02-07 19:35:21 +0530

    init

---
+++ GoogleVision.js
@@ -0,0 +1,41 @@
+const API_KEY = '<Enter the API key here >'; 
+const API_URL = `https://vision.googleapis.com/v1/images:annotate?key=${API_KEY}`;
+function generateBody(image) {
+  const body = {
+    requests: [
+      {
+        image: {
+          content: image,
+        },
+        features: [
+          {
+            type: 'TEXT_DETECTION',// Refer the docs for the paramas 
+            maxResults: 1,
+          },
+        ],
+      },
+    ],
+  };
+  return body;
+}
+
+async function callGoogleVisionAsync(image) {
+    return new Promise(async(resolve, reject) => {
+        const body = generateBody(image); //pass in our image for the payload
+        const response = await fetch(API_URL, {
+        method: 'POST',
+        headers: {
+            Accept: 'application/json',
+            'Content-Type': 'application/json',
+        },
+        body: JSON.stringify(body),
+        });
+        response.json().then((res)=>{
+            resolve(res)
+        }).catch((error)=>{
+            reject(error)
+        })
+    });
+}
+
+export default callGoogleVisionAsync;
---
+++ readme.md
@@ -0,0 +1,22 @@
+# ocr
+
+Android      |  iOS
+:-------------------------:|:-------------------------:
+![Android](https://github.com/generic-matrix/ocr-ml-kit/blob/main/output/output.gif?raw=true)  |  ![iOS](https://github.com/generic-matrix/ocr-ml-kit/blob/main/output/output2.gif?raw=true)
+
+# How to build the Android App
+
+# How to build the Android App
+
+* ```git clone https://github.com/generic-matrix/ocr-ml-kit.git```
+* cd react-native-app
+* npm install
+* react-native run-android
+
+
+# How To build iOS App
+
+* ```git clone https://github.com/generic-matrix/ocr-ml-kit.git```
+* cd react-native-app
+* npm install
+* react-native run-ios
